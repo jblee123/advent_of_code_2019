5d3f72f982e6a09f4ea5c20378b09b6c56ff4fc5
Brian Lee <brian.lee@gtri.gatech.edu>
2019-12-28 11:06:01 -0500

    Day09

---
+++ aoc2019_day09/src/day09a.rs
@@ -0,0 +1,13 @@
+pub mod day09_cpu;
+
+use aoc2019_utils;
+
+use day09_cpu::*;
+
+fn main() {
+    let input = aoc2019_utils::get_input("inputs/day09.txt");
+    let prog = parse_prog(&input);
+    let mut cpu = Cpu::new(&prog);
+    cpu.add_input(1);
+    cpu.exec_prog();
+}
